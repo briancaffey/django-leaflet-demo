d7e383f8d707946dff116ca077972d4077c482a6
Brian Caffey <briancaffey2010@gmail.com>
2018-02-23 14:48:42 -0500

    fixed test that was causing travis to fail

--- books/tests.py
+++ books/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Book
 from django.contrib.auth.models import User
+import datetime
 
 # Create your tests here.
 
@@ -10,7 +11,16 @@ class SimpleTest(TestCase):
         login = self.client.login(username='testuser', password='12345')
 
     def test_create_book(self):
-        book = Book.objects.create(title="Test Book")
+        book = Book.objects.create(
+            title="Test Book",
+            lon=43.432, 
+            lat=34.234,
+            publish_date=datetime.datetime.now(), 
+            pages=345,
+            website="http://www.mybook.com",
+            synopsis="This is the synopsis of the book.",
+            status=True)
+        book.save()
         all_books = Book.objects.all()
         self.assertEqual(len(all_books),1)
         first_book = all_books.first()
